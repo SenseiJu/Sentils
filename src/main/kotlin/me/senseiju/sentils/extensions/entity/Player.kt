fc83608e2b64a7d12c3b0691378fa86e44b5fdc2
SenseiJu <chathataranjit@yahoo.co.uk>
2021-06-26 00:45:13 +0100

    Updated Player.kt to now throw IllegalArgumentException instead of null

--- src/main/kotlin/me/senseiju/sentils/extensions/entity/Player.kt
+++ src/main/kotlin/me/senseiju/sentils/extensions/entity/Player.kt
@@ -5,6 +5,7 @@ import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.PlayerInventory
+import kotlin.IllegalArgumentException
 
 /**
  * Quicker way to play a sound to a player. Automatically sets the location to their current position
@@ -18,9 +19,10 @@ fun Player.playSound(sound: Sound, volume: Float = 1.0f, pitch: Float = 1.0f) {
  * Adds an item to the player's inventory. This will also drop any remaining items that did not fit into
  * the player's inventory onto the ground
  */
+@Throws(IllegalArgumentException::class)
 fun PlayerInventory.addItemOrDropNaturally(item: ItemStack, location: Location? = this.location) {
     if (location == null || location.world == null) {
-        throw NullPointerException(
+        throw IllegalArgumentException(
             "Cannot add item to inventory as the fallback drop location is null or the world is null."
         )
     }
