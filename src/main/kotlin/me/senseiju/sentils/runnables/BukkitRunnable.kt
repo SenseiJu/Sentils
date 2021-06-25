bfaf3cc20ae5bc7955ad5fd51a2626b888e086bf
SenseiJu <chathataranjit@yahoo.co.uk>
2021-06-26 00:53:26 +0100

    Made it clearer to see whats poppin

--- src/main/kotlin/me/senseiju/sentils/runnables/BukkitRunnable.kt
+++ src/main/kotlin/me/senseiju/sentils/runnables/BukkitRunnable.kt
@@ -5,10 +5,10 @@ import org.bukkit.scheduler.BukkitRunnable
 /**
  * Creates a new [BukkitRunnable]
  */
-fun newRunnable(block: () -> Unit): BukkitRunnable {
+fun newRunnable(toRun: () -> Unit): BukkitRunnable {
     return object : BukkitRunnable() {
         override fun run() {
-            block.invoke()
+            toRun()
         }
     }
 }
